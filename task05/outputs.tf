70455069e7b2a2cd123c100d754b403b9b849466
Nataliia <nataliashyngelska25@gmail.com>
2026-07-24 12:03:18 +0300

    task05

--- task05/outputs.tf
+++ task05/outputs.tf
@@ -1,3 +1,4 @@
 output "traffic_manager_fqdn" {
-  value = module.traffic_manager.traffic_manager_fqdn
+  description = "Traffic manager FQDN"
+  value       = module.traffic_manager.traffic_manager_fqdn
 }
--- task05/variables.tf
+++ task05/variables.tf
@@ -1,4 +1,5 @@
 variable "resource_groups" {
+  description = "Configuration of Azure Resource Groups."
   type = map(object({
     name     = string
     location = string
@@ -6,13 +7,12 @@ variable "resource_groups" {
 }
 
 variable "creator_tag" {
+  description = "Creator tag applied to all resources."
   type        = string
-  description = "Creator tag"
 }
 
-
-
 variable "app_service_plans" {
+  description = "Configuration of Azure App Service Plans."
   type = map(object({
     name         = string
     sku_name     = string
@@ -20,32 +20,32 @@ variable "app_service_plans" {
   }))
 }
 
-
 variable "app_services" {
+  description = "Configuration of Azure Windows Web Apps."
   type = map(object({
     name = string
   }))
 }
 
-
 variable "traffic_manager" {
+  description = "Configuration of the Azure Traffic Manager profile."
   type = object({
     profile_name   = string
     routing_method = string
   })
-
-  description = "Traffic Manager configuration"
 }
 
-
 variable "allow_ip_rule_name" {
-  type = string
+  description = "Name of the IP restriction rule."
+  type        = string
 }
 
 variable "allow_tag_rule_name" {
-  type = string
+  description = "Name of the Azure Traffic Manager service tag rule."
+  type        = string
 }
 
 variable "allowed_ip_address" {
-  type = string
+  description = "Public IP address allowed to access the web applications."
+  type        = string
 }
