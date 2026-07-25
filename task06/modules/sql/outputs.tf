39b98dea131a8fb865df2d398412966de4929463
Nataliia <nataliashyngelska25@gmail.com>
2026-07-25 14:22:41 +0300

    task06

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -1,5 +1,5 @@
 output "sql_server_fqdn" {
-  value = azurerm_mssql_server.sql_server.fully_qualified_domain_name
+  value       = azurerm_mssql_server.sql_server.fully_qualified_domain_name
   description = "SQL server fqdm"
 }
 
--- task06/modules/webapp/outputs.tf
+++ task06/modules/webapp/outputs.tf
@@ -1,4 +1,4 @@
 output "app_hostname" {
-  value = azurerm_linux_web_app.web_app.default_hostname
+  value       = azurerm_linux_web_app.web_app.default_hostname
   description = "Default app hostname"
 }
