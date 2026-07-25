5ede610e6ed745673d3af7e51e7f6d6759ef4c83
Nataliia <nataliashyngelska25@gmail.com>
2026-07-25 15:19:37 +0300

    task06

--- task06/modules/sql/outputs.tf
+++ task06/modules/sql/outputs.tf
@@ -5,7 +5,7 @@ output "sql_server_fqdn" {
 
 output "sql_connection_string" {
   description = "ADO.NET connection string для SQL Database"
-  value = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${random_password.admin_password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  value       = "Server=tcp:${azurerm_mssql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.sql_db.name};Persist Security Info=False;User ID=${var.sql_admin_username};Password=${random_password.admin_password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
   sensitive   = true
 }
 
