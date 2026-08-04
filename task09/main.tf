cba7fe316eaf06f03ebec88d8ec454f9f3489116
Nataliia <nataliashyngelska25@gmail.com>
2026-08-04 19:35:26 +0300

    task09

--- task09/main.tf
+++ task09/main.tf
@@ -8,8 +8,8 @@ module "afw" {
   location = data.azurerm_resource_group.rg.location
   rg_name  = data.azurerm_resource_group.rg.name
 
-  firewall_public_ip_name     = var.firewall_public_ip_name
-  aks_load_balancer_ip = var.aks_load_balancer_ip
+  firewall_public_ip_name = var.firewall_public_ip_name
+  aks_load_balancer_ip    = var.aks_load_balancer_ip
 
   aks_subnet_name      = var.aks_subnet_name
   virtual_network_name = var.virtual_network_name
